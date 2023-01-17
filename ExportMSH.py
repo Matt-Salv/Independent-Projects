6ec25529b3a1a34bc7727f01fabfbde17b7ff849
Matt-Salv <Petersmatt021@gmail.com>
2023-01-16 21:31:31 -0600

    ExportMSH attempt #1
    Updated to have blender info

--- ExportMSH.py
+++ ExportMSH.py
@@ -1,5 +1,19 @@
 import bpy
 
+bl_info = {
+    "name": "Dragon Nest MSH Exporter",
+    "author": "Matt-Salv",
+    "Version": (1, 0),
+    "blender": (2, 80, 0),
+    "location": "File > Import-Export",
+    "description": "Export DN model (.msh)",
+    "warning": "",
+    "wiki_url": "",
+    "support": 'COMMUNITY',
+    "category": "Import-Export",
+
+}
+
 obj = bpy.context.active_object #grab the active object
 
 vertices = obj.data.vertices #access vertices
@@ -9,11 +23,9 @@ uv_layers = obj.data.uv_layers #access UV coordinates
 materials = obj.material_slots #access material information
 obj.data.shape_keys.key_blocks #access bone weights
 
-obj = bpy.path.abspath("//") + obj.name + ".msh" #set path for export
-
-for obj in bpy.data.objects:
+for obj in bpy.data.objects: #loop through exporting all objects in scene
     if obj.type == 'MESH':
-        export_path = path + obj.name + ".msh"
+        export_path = bpy.path.abspath("//") + obj.name + ".msh"
         bpy.ops.export_mesh.msh(filepath=export_path)
 
 print("Done.")
