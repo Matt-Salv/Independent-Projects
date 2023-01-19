ccac4ba8bf9b2c4d4c17bc7b78af5e4f3d193a94
Matt-Salv <Petersmatt021@gmail.com>
2023-01-18 22:34:33 -0600

    More fixes...
    Can I still call them fixes if the script still doesn't work...? lmao

--- MSHExporter/__init__.py
+++ MSHExporter/__init__.py
@@ -1,16 +1,3 @@
-import bpy
-import bpy_extras.io_utils
-import struct
-import os
-from bpy.props import (
-    IntProperty,
-    BoolProperty,
-    StringProperty,
-    CollectionProperty,
-    EnumProperty,
-)
-from . exportmsh import MeshDataPointer
-
 bl_info = {
     "name": "Dragon Nest MSH Exporter",
     "author": "Matt-Salv",
@@ -24,27 +11,47 @@ bl_info = {
     "category": "Import-Export",
 }
 
-class Header:
+import bpy
+import bpy_extras.io_utils
+import struct
+import os
+import re
+from bpy.props import (
+    IntProperty,
+    BoolProperty,
+    StringProperty,
+    CollectionProperty,
+    EnumProperty,
+)
+
+class Header:               #Msh Header 1024 bytes
     def __init__(self):
         self.name = bytearray(256) #Eternity Engine Mesh File 0.1
         self.version = 0
         self.meshCount = 0
         self.unknown1 = 0x1
         self.unknown2 = 0x0
-        self.bbMax = [0.0, 0.0, 0.0]
         self.bbMin = [0.0, 0.0, 0.0]
-        self.boneCount = 0
-        self.unknown2 = 0x1 or 0x0
-        self.otherCount = 0
+        self.bbMax = [0.0, 0.0, 0.0]
+        self.boneCount = 0 #number of bones
+        self.Attributes = 0
+        self.AttachmentPoints = 0
+        self.empty = bytearray(768 - 52)
 
-class BoneData:
+    def set_version(self):
+        version_string = self.name[192:256:].decode("utf-8")
+        version_match = re.search(r"(\d+\.\d+)", version_string)
+        if version_match:
+            self.version = float(version_match.group(1))
+
+class BoneData:             #256 + 64 / bone
     def __init__(self):
         self.boneName = bytearray(256) #Bone name
         self.transformMatrix = [[0.0 for _ in range(4)] for _ in range(4)]
 
-class MeshInfo:
+class MeshInfo:             #Mesh entry header 1024 bytes
     def __init__(self):
-        self.sceneName = bytearray(256)
+        self.sceneName = bytearray(256) 
         self.meshName = bytearray(256)
         self.vertexCount = 0
         self.indexCount = 0
@@ -53,16 +60,56 @@ class MeshInfo:
         self.empty = bytearray(512 - 16)
 
 class MeshDataPointer:
-    def __init__(self, bone_data_list):
+    def __init__(self, header):
         self.pFaceIndex = None  # 0x2(unsigned short) * indexCount
         self.pVertexData = None  # 0x4(float) * 3 * vertexCount
         self.pNormalData = None  # 0x4(float) * 3 * vertexCount
         self.pUVData = None  # 0x4(float) * 2 * vertexCount
-        self.pBoneIndex = None  # 0x2(unsigned short) * 4 vertexCount
-        self.pBoneWeight = None  # 0x4(float) * 4 * vertexCount
-        self.pBoneCount = None  # The number of Bone bound by Mesh
-        self.pBoneName = None  # 0x100(char [256]) * boneCount
-        self.bone_data_list = bone_data_list
+        self.pUnknown3 = None  # 4x[V] int[V] unknown3
+        self.pBoneIndex = None  # 2x4x[V] Short[4][V] bone indices
+        self.pBoneWeight = None  # 4x4x[V] Float[4][V] bone weights
+        self.pBoneCount = None  # The number of Bone names[N] bound by Mesh
+        self.pBoneName = None  # 256x[N] FLString[N] bone names
+        self.attribute_type = None # 4 Int attribute type
+        self.attribute_type_0 = None  # 4x15 Float[15] Unknown (if attribute type 0)
+        self.attribute_type_1 = None  # 4x4 Float[4] Unknown (if attribute type 1)
+        self.attribute_type_2 = None  # 4x7 Float[7] Unknown (if attribute type 2)
+        self.attribute_type_3 = None  # 4x[3E]x9 Float[3x3][3E] Unknown 3x3 matrices (if attribute type 3)
+        self.attributes = []
+        self.attachment_points = []
+        if header.version >= 0.11:
+            for i in range (header.Attributes):
+                attribute = Attribute()
+                self.attributes.append(attribute) # 4xE int[E] attribute type
+        for i in range(header.Attributes):
+            attribute = Attribute()
+            self.attributes.append(attribute)
+
+class Attribute:
+    def __init__(self):
+        self.attribute_type = None
+        if self.attribute_type is None:
+            self.attribute_type = 0
+            print(f"{self.name} type not set, default value of 0 used")
+        self.parent_name = bytearray(256)
+        self.translation = [0.0, 0.0, 0.0]
+        self.unknown0 = [0.0 for _ in range(15)]
+        self.unknown1 = [0.0 for _ in range(4)]
+        self.unknown2 = [0.0 for _ in range(7)]
+        self.unknown3_matrices = [[[0.0 for _ in range(3)] for _ in range(3)] for _ in range(3)]
+        if Header.version >= 0.12:
+            self.parent_name = bytearray(256)  # FLString parent name (if msh.Version >= 11)
+            self.translation = [0.0,0.0,0.0]
+            if Header.version >= .13:
+                Attribute.transform_matrix = [[0.0 for _ in range(4)] for _ in range(4)]
+                self.attributes.append(Attribute)
+
+class AttachmentPoint:
+    def __init__(self):
+        self.name = bytearray(256) #FLString attachment name
+        self.translation = [0.0, 0.0, 0.0] # 4x3 Float[3] Translation (if msh.Version >=12)
+        self.parent_bone_name = bytearray(256) #FLString parent bone name (if msh.version >=13)
+        self.transform_matrix = [[0.0 for _ in range(4)] for _ in range(4)] #4x4x4 Float[4x4] Transform matrix (if msh.Version >= 13)
 
 class Vec2F:
     def __init__(self, vector = None):
@@ -101,20 +148,10 @@ class ExportMSH(bpy.types.Operator):
         if not bpy.context.selected_objects:
             self.report({'WARNING'}, "No objects selected.")
             return {'CANCELLED'}
-        for obj in context.selected_objects:
-            if obj.type == 'MESH':
-                print("Selected object is a mesh.")
-                mesh = obj
-            elif obj.type == 'ARMATURE':
-                armature = obj
-                print("Selected object is an armature.")
-            # add additional checks for other object types here
-            else:
-                print("Selected object is of an unknown type.")
-        
-        header = Header()
-        header.name = b'Eternity Engine Mesh File 0.1'
-        header.version = 1
+
+        header = bpy.context.object.header
+        header.set_version()
+        header.name = bytearray(256)
         header.meshCount = len(bpy.context.selected_objects)
         header.unknown1 = 1
         header.unknown2 = 0
@@ -122,9 +159,39 @@ class ExportMSH(bpy.types.Operator):
         header.bbMin = [0.0, 0.0, 0.0]
         header.boneCount = 0
         header.otherCount = 0
+        if context.selected_objects:
+            print("context.selected_objects is not empty")
+        for obj in context.selected_objects:
+            print(bpy.types.Object.header)
+            print(dir(bpy.types.Object))
+            print(dir(bpy.types))
+            if not hasattr(bpy.types.Object, "header"):
+                bpy.types.Object.header = bpy.props.PointerProperty(type=Header)
+            if not hasattr(bpy.types.Object, "bone_data"):
+                bpy.types.Object.bone_data = bpy.props.PointerProperty(type=BoneData)
+            if not hasattr(bpy.types.Object, "mesh_info"):
+                bpy.types.Object.mesh_info = bpy.props.PointerProperty(type=MeshInfo)
+            if not hasattr(bpy.types.Object, "mesh_data_pointer"):
+                bpy.types.Object.mesh_data_pointer = bpy.props.PointerProperty(type=MeshDataPointer)
+        else:
+            print("context.selected_objects is empty")
+            
+            if obj.type == 'MESH':
+                print("Selected object is a mesh.")
+                obj = bpy.context.object
+                mesh = obj
+                obj.header = Header()
+                if obj.header.version > header.version:
+                    header.version = obj.msh.version
+            elif obj.type == 'ARMATURE':
+                armature = obj
+                print("Selected object is an armature.")
+            # add additional checks for other object types here
+            else:
+                print("Selected object is of an unknown type.")
             
         meshes = []
-        bone_data_list = []
+        bone_data_list = [] 
         if armature:
             for bone in armature.data.edit_bones:
                 bone_data = BoneData()
@@ -143,6 +210,7 @@ class ExportMSH(bpy.types.Operator):
             meshes.append(mesh)
         header.boneCount = len(bone_data_list)
         mesh_data_pointer = MeshDataPointer(bone_data_list)
+        mesh_data_pointer.bone_data_list = bone_data_list
         mesh_info_list = []
         for mesh in meshes:
             mesh_info = MeshInfo()
@@ -225,7 +293,6 @@ class ExportMSH(bpy.types.Operator):
                 f.write(mesh_data_pointer.pBoneWeight)
                 f.write(struct.pack("i", mesh_data_pointer.pBoneCount))
                 f.write(mesh_data_pointer.pBoneName)
-
 
 def menu_func_export(self, context):
     self.layout.operator(ExportMSH.bl_idname,
