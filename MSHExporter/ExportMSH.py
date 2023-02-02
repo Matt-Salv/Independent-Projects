b7502b8c40a62f13d04dae7ad4511b458c484a0f
Matt-Salv <Petersmatt021@gmail.com>
2023-02-01 22:26:55 -0600

    Updates
    Finally some real progress. Need to figure out how to ascertain more correct information for the transform matrices of each bone.

--- MSHExporter/ExportMSH.py
+++ MSHExporter/ExportMSH.py
@@ -14,7 +14,7 @@ from bpy.props import (
 
 class HeaderClass:               #Msh Header 1024 bytes
     def __init__(self, name, version, meshCount, unknown1, unknown2, bbMin, bbMax, boneCount, Attributes, AttachmentPoints, empty):
-        self.name = name  #Eternity Engine Mesh File 0.1
+        self.name = name #Eternity Engine Mesh File 0.1
         self.version = version #version number (0.1 = version 10, 0.11 = 11, etc.)
         self.meshCount = meshCount #Number of meshes/entries [E]
         self.unknown1 = unknown1 #unsure the type, 4 bytes
@@ -31,69 +31,62 @@ class BoneData:             #256 + 64 / bone
         self.boneName = bytearray(256) #Bone name
         self.transformMatrix = [[0.0 for _ in range(4)] for _ in range(4)]
 
-class MeshInfo:             #Mesh entry header 1024 bytes
     def __init__(self):
-        self.sceneName = bytearray(256) 
-        self.meshName = bytearray(256)
-        self.vertexCount = 0
-        self.indexCount = 0
-        self.unknown = 0
-        self.renderMode = 0
-        self.empty = bytearray(512 - 16)
-
-class MeshDataPointer:
-    def __init__(self, Header):
-        self.pFaceIndex = None  # 0x2(unsigned short) * indexCount
-        self.pVertexData = None  # 0x4(float) * 3 * vertexCount
-        self.pNormalData = None  # 0x4(float) * 3 * vertexCount
-        self.pUVData = None  # 0x4(float) * 2 * vertexCount
-        self.pUnknown3 = None  # 4x[V] int[V] unknown3
-        self.pBoneIndex = None  # 2x4x[V] Short[4][V] bone indices
-        self.pBoneWeight = None  # 4x4x[V] Float[4][V] bone weights
-        self.pBoneCount = None  # The number of Bone names[N] bound by Mesh
-        self.pBoneName = None  # 256x[N] FLString[N] bone names
-        self.attribute_type = None # 4 Int attribute type
-        self.attribute_type_0 = None  # 4x15 Float[15] Unknown (if attribute type 0)
-        self.attribute_type_1 = None  # 4x4 Float[4] Unknown (if attribute type 1)
-        self.attribute_type_2 = None  # 4x7 Float[7] Unknown (if attribute type 2)
-        self.attribute_type_3 = None  # 4x[3E]x9 Float[3x3][3E] Unknown 3x3 matrices (if attribute type 3)
-        self.attributes = []
-        self.attachment_points = []
-        if Header.version >= 0.11:
-            for i in range (Header.Attributes):
-                attribute = Attribute()
-                self.attributes.append(attribute) # 4xE int[E] attribute type
-        for i in range(Header.Attributes):
-            attribute = Attribute()
-            self.attributes.append(attribute)
+        self.SceneName = bytearray(256) 
+        self.MeshName = bytearray(256)
+        self.NumVertices = 0
+        self.NumIndices = 0
+        self.Unknown = 0
+        self.Bitflag = 0
+        self.Padding = bytearray(496) #Including this line, and the preceding ones, this should = 1024 bytes.
+        self.FaceIndices = None  # Size 2x[I] short[I]
+        self.Vertices = None  # Size 4x[V] Float[V]
+        self.Normals = None  # size 4x[V] Float[V]
+        self.TextureCoords = None  # Size 4x[V] Float[V]
+        self.Unknown3 = None  # Size 4x[V] Int[V]
+        self.BoneIndices = None  # 2x4x[V] Short[4][V]
+        self.BoneWeights = None  # 4x4x[V] Float[4][V]
+        self.NumBoneNames = 0
+        self.BoneNames = []  # size 256x[N] FLString[N].
 
 class Attribute:
-    def __init__(self):
-        self.attribute_type = None
-        if self.attribute_type is None:
-            self.attribute_type = 0
-            print(f"{self.name} type not set, default value of 0 used")
-        self.parent_name = bytearray(256)
-        self.translation = [0.0, 0.0, 0.0]
-        self.unknown0 = [0.0 for _ in range(15)]
-        self.unknown1 = [0.0 for _ in range(4)]
-        self.unknown2 = [0.0 for _ in range(7)]
-        self.unknown3_matrices = [[[0.0 for _ in range(3)] for _ in range(3)] for _ in range(3)]
-        if HeaderClass.version >= 0.12:
-            self.parent_name = bytearray(256)  # FLString parent name (if msh.Version >= 11)
-            self.translation = [0.0,0.0,0.0]
-            if HeaderClass.version >= .13:
-                Attribute.transform_matrix = [[0.0 for _ in range(4)] for _ in range(4)]
-                self.attributes.append(Attribute)
+    def __init__(self, attachment_points):
+        self.attributes = []
+        self.header = HeaderClass()
+        for i in range(attachment_points):
+            attribute = Attribute(i)
+            self.attributes.append(attribute)
+    
+    class Attribute:
+        def __init__(self, index):
+            self.attribute_type = None
+            self.parent_name = bytearray(256)
+            self.unknown0 = [0.0 for _ in range(15)]
+            self.unknown1 = [0.0 for _ in range(4)]
+            self.unknown2 = [0.0 for _ in range(7)]
+            self.unknown3_matrices = [[[0.0 for _ in range(3)] for _ in range(3)] for _ in range(3)]
+            
+            if self.header.version >= 0.11:
+                self.parent_name = bytearray(256)
+                self.translation = [0.0, 0.0, 0.0]
+            
+            if self.header.version >= 0.12:
+                self.transform_matrix = [[0.0 for _ in range(4)] for _ in range(4)]
 
 class AttachmentPoint:
     def __init__(self):
-        self.name = bytearray(256) #FLString attachment name
-        self.translation = [0.0, 0.0, 0.0] # 4x3 Float[3] Translation (if msh.Version >=12)
-        self.parent_bone_name = bytearray(256) #FLString parent bone name (if msh.version >=13)
-        self.transform_matrix = [[0.0 for _ in range(4)] for _ in range(4)] #4x4x4 Float[4x4] Transform matrix (if msh.Version >= 13)
+        self.name = bytearray(256)
+        self.translation = [0.0, 0.0, 0.0]
+        self.parent_bone_name = bytearray(256)
+        self.transform_matrix = [[0.0 for _ in range(4)] for _ in range(4)]
+        
+        if HeaderClass.version >= 0.12:
+            self.translation = [0.0,0.0,0.0]
+        if HeaderClass.version >= 0.13:
+            self.parent_bone_name = bytearray(256)
+            self.transform_matrix = [[0.0 for _ in range(4)] for _ in range(4)]
 
-class Vec2F:
+'''class Vec2F:
     def __init__(self, vector = None):
         if vector is None:
             self.x = 0.0
@@ -116,4 +109,4 @@ class Vec4F(Vec3F):
         if vector is None:
             self.w = 0.0
         else:
-            self.w = vector.w 
+            self.w = vector.w'''
--- MSHExporter/__init__.py
+++ MSHExporter/__init__.py
@@ -7,10 +7,12 @@ from bpy.props import (
     CollectionProperty,
     EnumProperty,
 )
+from bpy_extras.io_utils import ExportHelper
 import struct
 import os
 import re
 import sys
+from . binary_reader import *
 # Add the directory containing the ExportMSH.py file to sys.path
 dir_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(dir_path, 'MSHExporter'))
@@ -32,13 +34,7 @@ bl_info = {
 }
 
 #!!!!! IMPORTANT: RENAME THE BLEND FILE NAME TO MATCH THE DESIRED .MSH FILE NAME !!!!!!!
-file_path = bpy.data.filepath
-def readMSH(file_path):
-    with open(file_path, "rb") as originalFile:
-        version = read_int(originalFile, num=1)
-        print(version)
-
-class ExportMSH(bpy.types.Operator, bpy_extras.io_utils.ExportHelper):
+class ExportMSH(bpy.types.Operator, ExportHelper):
     bl_idname = "export_msh.dragon_nest"
     bl_label = "Export Dragon Nest Model"
     bl_options = {'PRESET', 'UNDO'}
@@ -62,9 +58,89 @@ class ExportMSH(bpy.types.Operator, bpy_extras.io_utils.ExportHelper):
         maxlen=1024,
         default="",
     )
-
+    export_target: EnumProperty(name="Export Target",
+                                description="What to export.",
+                                items=(
+                                    ('SCENE', "Scene", "Export the current active scene."),
+                                    ('SELECTED', "Selected", "Export the currently selected objects and their parents."),
+                                    ('SELECTED_WITH_CHILDREN', "Selected with Children", "Export the currently selected objects with their children and parents.")
+                                ),
+                                default='SCENE')
     def execute (self, context):
-        readMSH()
+        if 'SELECTED' in self.export_target and len(bpy.context.selected_objects) == 0:
+            raise Exception("{} was chosen, but you have not selected any objects. "
+                            " Don't forget to unhide all the objects you wish to select!".format(self.export_target))  
+        with open(self.filepath, 'rb') as f:
+            header_data = f.read(1024)
+            name, version, meshCount, unknown1, unknown2, bbMin_x, bbMin_y, bbMin_z, bbMax_x, bbMax_y, bbMax_z, boneCount, Attributes, AttachmentPoints, padding = struct.unpack("256s4i3f3fii4s716s", header_data)
+            empty = padding + b"\0" * 716
+            bbMin = (bbMin_x, bbMin_y, bbMin_z)
+            bbMax = (bbMax_x, bbMax_y, bbMax_z)
+            header = HeaderClass(name, version, meshCount, unknown1, unknown2, bbMin, bbMax, boneCount, Attributes, AttachmentPoints, empty)
+            f.seek(1344)
+            firstBoneData = BoneData()
+            firstBoneData.boneName = f.read(256)
+            firstBoneData.transformMatrix = f.read(64)
+            firstBoneData.boneName = struct.unpack("256s", firstBoneData.boneName)[0].strip(b'\0')
+            firstBoneData.transformMatrix = struct.unpack("16f", firstBoneData.transformMatrix)
+            print("First Bone Name:", firstBoneData.boneName)
+            print("First Bone T-Matrix:", firstBoneData.transformMatrix)
+            
+        
+        file_name, file_ext = os.path.splitext(self.filepath)
+        new_file_name = file_name + "_UPDATED" + file_ext
+        meshCount = len(bpy.data.meshes)
+        armatures = [obj for obj in bpy.data.objects if obj.type == 'ARMATURE']
+        boneCount = sum(len(armature.data.bones) for armature in armatures)
+        '''To access the number of attributes and attachment points, you will need to know what data structure stores this information in Blender, 
+        and what the names of these variables are. You may need to consult Blender's API documentation or ask the developers of the file format you
+         are trying to export to.
+         COME BACK TO THIS LATER'''
+        
+        bone_data_list = []
+        for armature in armatures:
+            for bone in armature.data.bones:
+                bone_data = BoneData()
+                bone_data.boneName = bone.name.encode()[:256]
+                bone_matrix = armature.matrix_world @ bone.matrix_local
+                bone_data.transformMatrix = [list(row) for row in bone_matrix]
+                bone_data_list.append(bone_data)
+
+
+        with open(new_file_name, "wb") as outfile:    
+            #Write MSH Header information to file
+            outfile.write(struct.pack("256s4i3f3fii4s716s", header.name, header.version, header.meshCount, header.unknown1, header.unknown2, header.bbMin[0], header.bbMin[1], header.bbMin[2], header.bbMax[0], header.bbMax[1], header.bbMax[2], header.boneCount, header.Attributes, header.AttachmentPoints, header.empty))
+            #Write bone name and transform matrix to file, working but incorrectly... need to figure out why values from OG file are different
+            for armature in armatures:
+                for bone in armature.data.bones:
+                    bone_data = BoneData()
+                    bone_data.boneName = bone.name.encode("utf-8").ljust(256, b'\0')
+                    bone_data.transformMatrix = armature.matrix_world @ bone.matrix_local
+                    bone_matrix = list(bone_data.transformMatrix)
+                    bone_matrix[1][:3] = [-bone_matrix[1][0], -bone_matrix[1][1], -bone_matrix[1][2]]
+                    bone_matrix[2][:3] = [-bone_matrix[2][0], -bone_matrix[2][1], -bone_matrix[2][2]]
+                    bone_matrix[0][1], bone_matrix[1][0] = bone_matrix[1][0], bone_matrix[0][1]
+                    bone_matrix[0][2], bone_matrix[2][0] = bone_matrix[2][0], bone_matrix[0][2]
+                    bone_matrix[1][2], bone_matrix[2][1] = bone_matrix[2][1], bone_matrix[1][2]
+                    bone_data.transformMatrix = bone_matrix
+                    outfile.write(bone_data.boneName)
+                    outfile.write(struct.pack("16f", *[x for y in bone_data.transformMatrix for x in y]))
+                    print("Bone name:", bone.name)
+                    print("Bone transform matrix:", bone_data.transformMatrix)
+
+        
+        print("Name:", name)
+        print("Version:", version)
+        print("Mesh count:", meshCount)
+        print("Unknown1:", unknown1)
+        print("Unknown2:", unknown2)
+        print("Bounding box min:", bbMin)
+        print("Bounding box max:", bbMax)
+        print("Bone count:", boneCount)
+        print("Attributes:", Attributes)
+        print("Attachment points:", AttachmentPoints)
+        print("Empty:", empty)
+        print("Done")
 
 def menu_func_export(self, context):
     self.layout.operator(ExportMSH.bl_idname,
